80ccc522580f15a6c3ae7701f48bda45e2059271
yehaodong <yehaodong@genomics.cn>
2023-03-24 18:00:40 +0800

    BGI blue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <n-config-provider :theme="theme">
+  <n-config-provider :theme-overrides="themeOverrides">
     <n-global-style />
     <div class="w-full lg:w-screen-lg mx-auto h-screen flex flex-col">
       <div class="my-4">
@@ -8,15 +9,26 @@
       <router-view></router-view>
     </div>
   </n-config-provider>
+  </n-config-provider>
+
 </template>
 
 <script setup lang="ts">
 import PageHeader from './components/PageHeader.vue';
-import { darkTheme } from 'naive-ui'
+import { darkTheme, GlobalThemeOverrides } from 'naive-ui'
 import { useAppStore } from './store';
 import { computed } from 'vue';
 
 const appStore = useAppStore();
+
+const themeOverrides: GlobalThemeOverrides = {
+    common: {
+    primaryColor: '#005bab',
+    primaryColorHover: '#0083f7',
+    primaryColorPressed: '#0067c3',
+    primaryColorSuppl: '#005bab'
+    }
+  }
 
 const theme = computed(() => {
   if (appStore.theme == 'dark') {
